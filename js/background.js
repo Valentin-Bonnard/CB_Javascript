aab38b51bea5194a0a882c7abd31994ca18fcf71
Valentin-Bonnard <valentinbonnard0303@gmail.com>
2016-09-16 10:15:34 +0200

    1er commit
    Jeu navigateur (test) en javascript ||  niveau cabine doctor who + dalek
    en pixel art (avec beaucoup d'imagination ) + space invader (jeu sans
    bonus )

---
+++ js/background.js
@@ -0,0 +1,8 @@
+var circlesCount = 100; // Nombres de cercles dans le trou de ver.
+var offsetX = 70; // Decalage du trou de ver vers x 
+var offsetY = 40; // Decalage du trou de ver vers y 
+var maxDepth = 1.5; // Distance maximale d'un cercle
+var circleDiameter = 10.0; // Diametre d'un cercle 
+var dephtSpedd = 0.001; // Vitesse de rapprochement d'un cercle 
+var angleSpeed = 0.05; // Vitesse de rotation du cercle 
+
